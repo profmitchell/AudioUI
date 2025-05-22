8cffb11cc5c7570096b1e8941b5f23b8f328fd31
Mitchell Cohen <122713703+profmitchell@users.noreply.github.com>
2025-05-22 10:58:15 -0400

    Configure for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,10 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/AudioUI' : '',
+  // The repository name (AudioUI)
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/AudioUI/' : '',
 }
 
 export default nextConfig
